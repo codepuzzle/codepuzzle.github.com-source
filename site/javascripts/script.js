6ecc29fb6dc07114081263753d4c60f929cf2e95
Anton Mikulcic <anton@codepuzzle.de>
2010-08-22 14:22:13 +0200

    added puff-navigation

--- site/javascripts/script.js
+++ site/javascripts/script.js
@@ -1,26 +1,17 @@
-/* Author: 
+$(document).ready(function() {
 
-*/
+  $('div.post:first').show();
+  $('div.post a.continue').click(function(e) {
+    e.preventDefault();
+    var current = $(this).parents('div.post');
+    var next = current.next('div.post');
+    if (next.size() == 0) {
+      next = $('div.post:first');
+    }
+    current.css({ width: current.width(), position: 'absolute' });
+    $('div.post:visible').hide('puff', {}, 500);
+    next.css({ position: 'static' });
+    next.show('slide', { direction: 'up' }, 1000);
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
