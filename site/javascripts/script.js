49abf4e1e09c66a94c31b601e6b4d4732850818b
Anton Mikulcic <anton@codepuzzle.de>
2010-08-23 10:55:34 +0200

    v1!

--- site/javascripts/script.js
+++ site/javascripts/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+  $.browser.mobile = /(IEMobile|Windows CE|NetFront|PlayStation|PLAYSTATION|like Mac OS X|MIDP|UP\.Browser|Symbian|Nintendo|Android)/.test(navigator.userAgent);
+
+  //
+
   $('div.post:first').show();
   $('div.post a.continue').click(function(e) {
     e.preventDefault();
@@ -8,10 +12,38 @@ $(document).ready(function() {
     if (next.size() == 0) {
       next = $('div.post:first');
     }
-    current.css({ width: current.width(), position: 'absolute' });
-    $('div.post:visible').hide('puff', {}, 500);
-    next.css({ position: 'static' });
-    next.show('slide', { direction: 'up' }, 1000);
+    if ($.browser.mobile) {
+      current.hide();
+      next.show();
+    } else {
+      current.css({ width: current.width(), position: 'absolute' });
+      $('div.post:visible').hide('puff', {}, 500);
+      next.css({ position: 'static' });
+      next.show('slide', { direction: 'up' }, 1000);
+    }
   });
+
+  //
+
+  if ($.browser.mobile) {
+    //
+  } else if ($.browser.webkit) {
+    $('.eyecatcher span').hover(function() {
+      $(this).delay(500).hide();
+    }, function() {
+      $(this).clearQueue().show();
+    });
+  } else {
+    $('.eyecatcher a').hover(function() {
+      $(this).find('span').show().css({
+        visibility: 'visible',
+        opacity: 0.4,
+        fontSize: '40px'
+      }).animate({  
+        opacity: 0,
+        fontSize: '150px'
+      }, 300, function() { $(this).hide(); });
+    }, function() {});
+  }
 
 });
